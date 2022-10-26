71af2656c80d78d9e6d540e8c2f3c13a0ef2cf15
Ak627 <91678317+Ak627@users.noreply.github.com>
2022-10-26 09:53:00 -0600

    Create Histogram.py

---
+++ Histogram.py
@@ -0,0 +1,26 @@
+from matplotlib import pyplot
+import numpy
+
+#randint() will generate 20 numbers from 1-99, store output in list named "numList"
+numList = numpy.random.randint(200, size = 50)
+
+#print to console for testing
+print("the list of numbers we generated is:", numList)
+
+#this creates a graphical window of size x inches by y inches
+screen = pyplot.figure(figsize = (12,8))
+
+#1)set up a histogram, let pyplot use default bins
+#pyplot.hist(numList)
+#2)set up a histogram, set number of bins:
+#pyplot.hist(numList, 10)
+#3) set up a histogram, set edges of bins:
+for i in range(10):
+    pyplot.hist(numList, bins = [0,20,40,60,80, 100, 120, 130, 140, 160, 180,200])
+
+#set title
+pyplot.title("Professionally professional histogram")
+
+#show plot
+
+pyplot.show()
